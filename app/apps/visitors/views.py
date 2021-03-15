9d406239f61db83f9c11f45136c7633d05d87eb9
Janis Goesser <jgoesser@ailio.de>
2021-03-15 02:17:54 +0100

    modify date planner and add day detail overview to views

--- app/apps/visitors/views.py
+++ app/apps/visitors/views.py
@@ -11,39 +11,67 @@ from .models import Visitor
 BOOKING_RULES = settings.BOOKING_RULES
 
 
+def get_datetime_from_date_and_time_objects(date, time):
+    return datetime(year=date.year, month=date.month, day=date.day,
+                    hour=time.hour, minute=time.minute)
+
+
+def datetime_is_not_expired(timestamp, closing_time):
+    return (
+        timestamp + timedelta(minutes=BOOKING_RULES["expected_residence"]) <= closing_time
+    )
+
+
+def get_early_exit_timepair_from_timestamp(timestamp):
+    early_datetime = timestamp - timedelta(minutes=BOOKING_RULES["expected_residence"])
+    exit_datetime = timestamp + timedelta(minutes=BOOKING_RULES["expected_residence"])
+    return early_datetime, exit_datetime
+
+
+def timestamp_is_bookable(timestamp):
+    early_datetime, exit_datetime = get_early_exit_timepair_from_timestamp(timestamp)
+    visitors = Visitor.objects.filter(
+        Q(planed_entry__gt=early_datetime) & Q(planed_entry__lt=exit_datetime)
+    )
+    return visitors.count() < BOOKING_RULES["max_visitor_capacity"]
+
+
 class TerminAPI(GenericAPIView):
-    def retrieve_day_planning(self, request, *args, **kwargs):
-        pass
+    def retrieve_day_planning(self, request, date, *args, **kwargs):
+        current_time = get_datetime_from_date_and_time_objects(
+            date=datetime.strptime(date, "%Y-%m-%d"),
+            time=BOOKING_RULES["opening"]
+        )
+        closing = get_datetime_from_date_and_time_objects(
+            date=datetime.strptime(date, "%Y-%m-%d"),
+            time=BOOKING_RULES["closing"]
+        )
+        times = []
+        while datetime_is_not_expired(timestamp=current_time, closing_time=closing):
+            is_bookable = timestamp_is_bookable(timestamp=current_time)
+            times.append({
+                "date": current_time.strftime("%Y-%m-%d"),
+                "time": current_time.strftime("%H:%M"),
+                "is_bookable": is_bookable
+            })
+            current_time = current_time + timedelta(minutes=BOOKING_RULES["frequence"])
+        return Response(data=times, status=status.HTTP_200_OK)
 
     def retrieve_date_planning(self, request, *args, **kwargs):
         current_timestamp = now()
         dates = []
         for day in range(BOOKING_RULES["max_days_in_future"]):
-            current_time = datetime(
-                year=current_timestamp.year,
-                month=current_timestamp.month,
-                day=current_timestamp.day,
-                hour=BOOKING_RULES["opening"].hour,
-                minute=BOOKING_RULES["opening"].minute
+            current_time = get_datetime_from_date_and_time_objects(
+                date=current_timestamp,
+                time=BOOKING_RULES["opening"]
             )
-            closing = datetime(
-                year=current_timestamp.year,
-                month=current_timestamp.month,
-                day=current_timestamp.day,
-                hour=BOOKING_RULES["closing"].hour,
-                minute=BOOKING_RULES["closing"].minute
+            closing = get_datetime_from_date_and_time_objects(
+                date=current_timestamp,
+                time=BOOKING_RULES["closing"]
             )
             is_bookable = False
-            while current_time + timedelta(minutes=BOOKING_RULES["expected_residence"]) < closing:
-                early_datetime = current_time - timedelta(minutes=BOOKING_RULES["inacuracy"])
-                exit_datetime = (
-                    current_time + timedelta(minutes=BOOKING_RULES["expected_residence"])
-                )
-                visitors = Visitor.objects.filter(
-                    Q(planed_entry__gte=early_datetime) &
-                    Q(planed_entry__lte=exit_datetime)
-                )
-                is_bookable = visitors.count() < BOOKING_RULES["max_visitor_capacity"]
+            while datetime_is_not_expired(timestamp=current_time, closing_time=closing):
+                is_bookable = timestamp_is_bookable(timestamp=current_time)
                 if is_bookable:
                     break
                 current_time = current_time + timedelta(minutes=BOOKING_RULES["frequence"])
@@ -57,5 +85,5 @@ class TerminAPI(GenericAPIView):
     def get(self, request, *args, **kwargs):
         date = request.GET.get("date", None)
         if date is not None:
-            return self.retrieve_day_planning(request, *args, **kwargs)
+            return self.retrieve_day_planning(request, date=date, *args, **kwargs)
         return self.retrieve_date_planning(request, *args, **kwargs)
